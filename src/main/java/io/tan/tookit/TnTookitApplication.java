be3978b66e9452200135b68697b3496eefa1c62c
tan <1445763190@qq.com>
2021-11-22 23:09:32 +0800

    init

---
+++ src/main/java/io/tan/tookit/TnTookitApplication.java
@@ -0,0 +1,13 @@
+package io.tan.tookit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnTookitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnTookitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tan/tookit/TnTookitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tan.tookit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnTookitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
