54a9c9e6e28d0c566322d77f92e2e9fc16e3d669
tn <1445763190@qq.com>
2021-11-24 10:15:43 +0800

    [优化] 删除多余

--- src/main/java/io/tan/tookit/util/CommandUtil.java
+++ src/main/java/io/tan/tookit/util/CommandUtil.java
@@ -4,7 +4,6 @@ import cn.hutool.core.io.IoUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
